390c5e7685d0df8c5949672005de3d65114333ba
Imaan Munir <imunir@umich.edu>
2017-10-06 23:15:34 -0400

    printing 6b final answer

--- homework6b.py
+++ homework6b.py
@@ -4,21 +4,20 @@ from bs4 import *
 website = input('Type in URL: ')
 repeat = int(input('How many times would you like to repeat this: '))
 position = int(input('Enter the position: ')) - 1
-taglist = list()
 urllist = list()
 urllist.append(website)
 
 print ('Retrieving: ', urllist[0])
 
-
+tag_list = list()
 for i in range(repeat):
     opening = ur.urlopen(urllist[-1]).read()
     soup = BeautifulSoup(opening,"lxml")
     tags = soup('a')
-    taglist = list()
+    tag_list = list()
     for tag in tags:
-        taglist.append(tag)
-    url = taglist[position].get('href', None)
+        tag_list.append(tag)
+    url = tag_list[position].get('href', None)
     print ('Retrieving: ', url)
     urllist.append(url)
 
